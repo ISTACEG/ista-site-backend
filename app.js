46f8b84b2871c3483d6f3fa7ed41e2419e1a4f59
YUVARAJ787 <yuvarajv787@gmail.com>
2024-12-26 10:55:31 +0530

    initial configuration

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const bodyParser = require('body-parser');
 const dbConnection = require('./config/dbConfig');
 const addUser = require('./routes/addUser');
+const posts = require('./routes/post');
 
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
 app.use('/auth', addUser);
+app.use('/post', addUser);
 
 app.listen(PORT, () => {
     console.log(`listening on http://localhost:${PORT}`);
--- models/user.js
+++ models/user.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: false },
-    password: { type: String, required: true, unique: false }
+    // template fields
 });
 
 const User = mongoose.model('User', userSchema);
